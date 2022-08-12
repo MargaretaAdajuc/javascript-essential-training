db97ae7b79670c83ad153d15e112b09429e6ed9f
Margareta Adajuc <margaretaadajuc@gmail.com>
2022-08-12 11:13:58 +0300

    created an object and a nested one for practice reasons

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,21 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const desk = {
+  name: "Work desk",
+  length: 100,
+  width: 70,
+  numberOfDrawers: 5,
+  drawerContents: {
+    agendaColour: "blue",
+    penColour: "black",
+    stickyNotesShape: "cloud",
+  },
+  drawerOpen: false,
+  toggleDrawer: function (drawerStatus) {
+    this.drawerOpen = drawerStatus;
+  },
+};
+
+console.log(desk);
