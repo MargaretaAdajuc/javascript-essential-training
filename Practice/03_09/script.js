4b83ea0b4d703368fc5f3931d511145339dc6cf4
Margareta Adajuc <margaretaadajuc@gmail.com>
2022-08-22 10:24:30 +0300

    created a method for each object property and then replaced them and viewed the new result in the console

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -23,4 +23,36 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  newName: function (newName) {
+    this.name = newName;
+  },
+  newVolume: function (newVolume) {
+    this.volume = newVolume;
+  },
+  newColor: function (newColor) {
+    this.color = newColor;
+  },
+  newPocketNum: function (newPocketNum) {
+    this.pocketNum = newPocketNum;
+  },
 };
+
+console.log("Left strap lenght before: ", backpack.strapLength.left);
+console.log("Right strap lenght before: ", backpack.strapLength.right);
+console.log("Name before: ", backpack.name);
+console.log("Volume before: ", backpack.volume);
+console.log("Color before: ", backpack.color);
+console.log("Number of pockets before: ", backpack.pocketNum);
+
+backpack.newStrapLength(20, 40);
+backpack.newName("Vacay backpack");
+backpack.newVolume("60");
+backpack.newColor("pink");
+backpack.newPocketNum(20);
+
+console.log("Left strap lenght after: ", backpack.strapLength.left);
+console.log("Right strap lenght after: ", backpack.strapLength.right);
+console.log("Name after: ", backpack.name);
+console.log("Volume after: ", backpack.volume);
+console.log("Color after: ", backpack.color);
+console.log("Number of pockets after: ", backpack.pocketNum);
