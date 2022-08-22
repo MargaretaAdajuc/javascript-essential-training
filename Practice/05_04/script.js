0132a447d627aed1e45946510b33dfdd3f05b4fd
Margareta Adajuc <margaretaadajuc@gmail.com>
2022-08-22 23:16:14 +0300

    selected different elements by class

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,15 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+document
+  .querySelectorAll("button")
+  .forEach((item) => (item.style.color = "magenta"));
+
+let feature = document.querySelectorAll(".feature");
+
+let color = (feature.document.querySelector(
+  ".feature backpack__color"
+).style.color = "YELLOW");
+
+console.log(color);
