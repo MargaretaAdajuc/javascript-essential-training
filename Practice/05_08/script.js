4579361ee68eac44b4b41ad9bfcab378b8364cc6
Margareta Adajuc <margaretaadajuc@gmail.com>
2022-08-23 22:10:50 +0300

    selected an element, added and removed classes and edited the style

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,20 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+let p = document.getElementsByTagName("p");
+
+p?.classList?.add("first");
+p?.classList?.add("paragraph");
+
+p?.classList?.remove("paragraph");
+
+let par = document.querySelector("p");
+
+par.setAttribute("title", "abc");
+
+par.getAttribute("title");
+
+par.setAttribute("title", "Backpack paragraph");
+
+par.style.color = "aquamarine";
